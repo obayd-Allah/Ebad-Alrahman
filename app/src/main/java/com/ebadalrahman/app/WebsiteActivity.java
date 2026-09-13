6f2b8b97eb36edfb6476d7068d769e51e171695a
obayd-Allah <heba20268484@gmail.com>
2026-09-13 22:16:18 +0300

    Create WebsiteActivity.java

---
+++ app/src/main/java/com/ebadalrahman/app/WebsiteActivity.java
@@ -0,0 +1,43 @@
+package com.ebadalrahman.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebsiteActivity extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl(
+                "https://obayd-allah.github.io/fafirru-ila-allah-tagroba/index.html?mosque=mosque_002"
+        );
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
